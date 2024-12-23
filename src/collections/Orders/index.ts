44c7941da8a6b04d3d193c1487efbc3cb2e5bd9b
John Tettis <johndavidtettis@gmail.com>
2024-12-22 19:34:52 -0500

    tabs added

--- src/collections/Orders/index.ts
+++ src/collections/Orders/index.ts
@@ -7,32 +7,109 @@ export const Orders: CollectionConfig = {
   },
   fields: [
     {
-      name: 'Order Number',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'DiscountCode',
-      type: 'relationship',
-      relationTo: 'discounts',
-    },
-    {
-      name: 'Products',
-      type: 'array',
-      fields: [
+      type: 'tabs',
+      tabs: [
         {
-          name: 'Product',
-          type: 'relationship',
-          relationTo: 'products',
-          required: true,
+          label: 'Order Details',
+          description: 'Products, discounts, etc',
+          fields: [
+            {
+              name: 'Order Number',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'DiscountCode',
+              type: 'relationship',
+              relationTo: 'discounts',
+            },
+            {
+              name: 'Products',
+              type: 'array',
+              fields: [
+                {
+                  name: 'Product',
+                  type: 'relationship',
+                  relationTo: 'products',
+                  required: true,
+                },
+                {
+                  name: 'Quantity',
+                  type: 'number',
+                  required: true,
+                },
+              ],
+              required: true,
+            },
+            {
+              name: 'Total',
+              type: 'number',
+            },
+            {
+              name: 'Order Date',
+              type: 'date',
+            },
+          ],
         },
         {
-          name: 'Quantity',
-          type: 'number',
-          required: true,
+          label: 'Shipping Information',
+          description: 'Address, status, etc',
+          fields: [
+            {
+              name: 'Shipping Address',
+              type: 'richText',
+              required: true,
+            },
+            {
+              name: 'Status',
+              type: 'select',
+              options: ['Pending', 'Shipped', 'Delivered'],
+              required: true,
+            },
+            {
+              name: 'Tracking Number',
+              type: 'text',
+            },
+            {
+              name: 'Shipping Cost',
+              type: 'number',
+            },
+          ],
+        },
+        {
+          label: 'Customer Information',
+          description: 'Contact and Payment information',
+          fields: [
+            {
+              name: 'Customer Name',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'Email',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'Phone Number',
+              type: 'text',
+              required: true,
+            },
+            {
+              name: 'Payment Method',
+              type: 'select',
+              options: ['Credit Card', 'Debit Card', 'Paypal', 'Cash'],
+              required: true,
+            },
+            {
+              name: 'Payment Status',
+              type: 'select',
+              options: ['Paid', 'Pending', 'Failed'],
+              required: true,
+            },
+          ],
         },
       ],
-      //   required: true,
     },
   ],
 }
