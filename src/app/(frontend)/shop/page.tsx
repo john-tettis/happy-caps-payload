cc5589910fcea540e07eaa966f080a4e140c0c22
John Tettis <johndavidtettis@gmail.com>
2024-12-24 09:58:01 -0500

    shop page. borrowed from the posts page

---
+++ src/app/(frontend)/shop/page.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from 'next/types'
+
+import { ProductList } from '@/components/ProductList'
+import { PageRange } from '@/components/PageRange'
+import { Pagination } from '@/components/Pagination'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import React from 'react'
+import PageClient from './page.client'
+
+export const dynamic = 'force-static'
+export const revalidate = 600
+
+export default async function Page() {
+  const payload = await getPayload({ config: configPromise })
+
+  const products = await payload.find({
+    collection: 'products',
+    depth: 1,
+    limit: 12,
+    overrideAccess: false,
+    select: {
+      title: true,
+      product_type: true,
+      price: true,
+      pictures: true,
+      description: true,
+      quantity: true,
+      slug: true,
+      categories: true,
+      meta: true,
+    },
+  })
+
+  return (
+    <div className="pt-24 pb-24">
+      <PageClient />
+      <div className="container mb-16">
+        <div className="prose dark:prose-invert max-w-none">
+          <h1>Happy Little Storefront</h1>
+        </div>
+      </div>
+
+      <div className="container mb-8">
+        <PageRange
+          collection="products"
+          currentPage={products.page}
+          limit={12}
+          totalDocs={products.totalDocs}
+        />
+      </div>
+      <ProductList products={products.docs} />
+
+      <div className="container">
+        {products.totalPages > 1 && products.page && (
+          <Pagination page={products.page} totalPages={products.totalPages} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export function generateMetadata(): Metadata {
+  return {
+    title: `Happy Little Storefront`,
+  }
+}
