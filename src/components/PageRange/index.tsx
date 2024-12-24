ee0a3ddf0c06bcdb80d697ba636e839f4ec7e363
John Tettis <johndavidtettis@gmail.com>
2024-12-24 10:00:21 -0500

    updated to be usable for products - I dont know why these things are hardcoded by default. I do not find a pagination component exceptionally useful if it only accepts one type of component

--- src/components/PageRange/index.tsx
+++ src/components/PageRange/index.tsx
@@ -10,6 +10,10 @@ const defaultCollectionLabels = {
     plural: 'Posts',
     singular: 'Post',
   },
+  products: {
+    plural: 'Products',
+    singular: 'Products',
+  },
 }
 
 export const PageRange: React.FC<{
