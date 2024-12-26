2310edf8e94ff3bead7c1ffc4c17eb3b525ae151
John Tettis <johndavidtettis@gmail.com>
2024-12-25 21:26:07 -0500

    added additional customization to the richtext field description

--- src/collections/Products/index.ts
+++ src/collections/Products/index.ts
@@ -1,8 +1,17 @@
 import type { CollectionConfig } from 'payload'
 import { authenticated } from '../../access/authenticated'
 import { anyone } from '../../access/anyone'
-import { HTMLConverterFeature, lexicalEditor, lexicalHTML } from '@payloadcms/richtext-lexical'
+import {
+  BlocksFeature, FixedToolbarFeature,
+  HeadingFeature, HorizontalRuleFeature,
+  HTMLConverterFeature, InlineToolbarFeature,
+  lexicalEditor,
+  lexicalHTML,
+} from '@payloadcms/richtext-lexical'
 import { slugField } from '@/fields/slug'
+import { Banner } from '@/blocks/Banner/config'
+import { Code } from '@/blocks/Code/config'
+import { MediaBlock } from '@/blocks/MediaBlock/config'
 export const Products: CollectionConfig = {
   slug: 'products',
   access: {
@@ -44,12 +53,17 @@ export const Products: CollectionConfig = {
       type: 'richText',
       required: true,
       editor: lexicalEditor({
-        features: ({ defaultFeatures }) => [
-          ...defaultFeatures,
-          // The HTMLConverter Feature is the feature which manages the HTML serializers.
-          // If you do not pass any arguments to it, it will use the default serializers.
-          HTMLConverterFeature({}),
-        ],
+        features: ({ rootFeatures }) => {
+          return [
+            ...rootFeatures,
+            HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
+            BlocksFeature({ blocks: [Banner, Code, MediaBlock] }),
+            FixedToolbarFeature(),
+            InlineToolbarFeature(),
+            HorizontalRuleFeature(),
+            HTMLConverterFeature({}),
+          ]
+        },
       }),
     },
     lexicalHTML('description', { name: 'description_html' }),
