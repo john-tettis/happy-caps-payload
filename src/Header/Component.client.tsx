e9bbd8ad11217beb06f84963ad504f47e549b363
John Tettis <johndavidtettis@gmail.com>
2024-12-26 19:17:14 -0500

    added background and made it a little shorter

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
+
 import type { Header } from '@/payload-types'
 
 import { Logo } from '@/components/Logo/Logo'
@@ -19,6 +20,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   const { headerTheme, setHeaderTheme } = useHeaderTheme()
   const pathname = usePathname()
 
+
   useEffect(() => {
     setHeaderTheme(null)
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -30,8 +32,8 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+    <header className="container relative z-20   w-screen bg-orange-400 " {...(theme ? { 'data-theme': theme } : {})}>
+      <div className="py-4 flex justify-start gap-20">
         <Link href="/">
           <Logo loading="eager" priority="high" className="" />
         </Link>
